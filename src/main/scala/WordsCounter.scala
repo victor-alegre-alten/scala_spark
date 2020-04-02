9077a9b0fed4147aa3c2101573d84fcf1292bb2b
victor-alegre-santos <victorvkr98@gmail.com>
2020-04-02 16:48:57 +0200

    Ordenado con dataframes

--- src/main/scala/WordsCounter.scala
+++ src/main/scala/WordsCounter.scala
@@ -1,7 +1,7 @@
 import org.apache.commons.lang3.StringUtils
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 
 class WordsCounter(text: RDD[String], val stopWords: Array[String]) extends Serializable {
 
@@ -25,6 +25,9 @@ class WordsCounter(text: RDD[String], val stopWords: Array[String]) extends Seri
       .map(sanitize)
       .map(word => word.toUpperCase())
       .filter(isValidWord)
+      // .map(word => (word, 1)) => (String, Int)
+      // .reduceByKey(_ + _)
+      // .sortBy(_._2, false)
   }
 }
 
@@ -33,11 +36,23 @@ class WordsCounter(text: RDD[String], val stopWords: Array[String]) extends Seri
 object WordCounter {
   def main(args: Array[String]): Unit = {
     val session = SparkSession.builder.master("local[2]").appName("WordCounter").getOrCreate()
+    import session.implicits._
+    import org.apache.spark.sql.functions._
+
     val stopWords: Array[String] = Array("A", "EL", "NUNCA")
     val texto = session.sparkContext.textFile("src/main/resources/texto.txt")
     val wordsCounter = new WordsCounter(texto, stopWords)
 
-    wordsCounter.calculate().foreach(println)
+    val calculatedRDD = wordsCounter.calculate()
+
+    val orderedDataFrame = calculatedRDD
+      .toDF("word")
+      .groupBy("word")
+      .count()
+      .orderBy(desc("count"))
+
+    orderedDataFrame.show()
+
 
     session.stop()
   }
