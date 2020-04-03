48e57af51e89011193d3b9dccd90bfd1868e1415
victor-alegre-santos <victorvkr98@gmail.com>
2020-04-03 11:00:45 +0200

    Añadido un LongAccumulator para sincronizar los datos de palabras eliminadas

--- src/main/scala/WordsCounter.scala
+++ src/main/scala/WordsCounter.scala
@@ -1,9 +1,11 @@
 import org.apache.commons.lang3.StringUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.util.LongAccumulator
 
-class WordsCounter(text: RDD[String], val stopWords: Array[String]) extends Serializable {
+class WordsCounter(text: RDD[String], val stopWords: Array[String], val totalDeleted: LongAccumulator)
+  extends Serializable {
 
   private def splitWords (words: String): Array[String] = {
     words
@@ -11,19 +13,21 @@ class WordsCounter(text: RDD[String], val stopWords: Array[String]) extends Seri
       .filter(word => !word.equals(""))
   }
 
-  private def sanitize(word: String): String = {
-    StringUtils.stripAccents(word)
-  }
-
   private def isValidWord(word: String): Boolean = {
-    !stopWords.contains(word)
+    val contains = stopWords.contains(word)
+
+    if (!contains){
+      totalDeleted.add(1)
+    }
+
+    !contains
   }
 
   def calculate (): RDD[String] = {
     text
       .flatMap(splitWords)
-      .map(sanitize)
-      .map(word => word.toUpperCase())
+      .map(StringUtils.stripAccents)
+      .map(_.toUpperCase)
       .filter(isValidWord)
       // .map(word => (word, 1)) => (String, Int)
       // .reduceByKey(_ + _)
@@ -38,9 +42,11 @@ object WordCounter {
     val session = SparkSession.builder.master("local[2]").appName("WordCounter").getOrCreate()
     import session.implicits._
 
-    val stopWords: Array[String] = Array("A", "EL", "NUNCA")
+    val stopWords: Array[String] = Array("A", "EL", "NUNCA", "CAPERUCITA")
     val texto = session.sparkContext.textFile("src/main/resources/texto.txt")
-    val wordsCounter = new WordsCounter(texto, stopWords)
+    val totalDeletedAcc: LongAccumulator = session.sparkContext.longAccumulator
+
+    val wordsCounter = new WordsCounter(texto, stopWords, totalDeletedAcc)
 
     val calculatedRDD = wordsCounter.calculate()
 
@@ -50,7 +56,10 @@ object WordCounter {
       .count()
       .orderBy(desc("count"))
 
+
     orderedDataFrame.show()
+    println("Eliminadas: " + wordsCounter.totalDeleted.value)
+
 
 
     session.stop()
